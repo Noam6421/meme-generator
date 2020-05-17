033167c48627b441a1310cc451b9c6e1e63f0f9b
Noam Korkos <noam6421@gmail.com>
2020-05-17 11:07:42 +0300

    with first design and reset button

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -18,25 +18,35 @@ const MemeGenerator = () => {
         const randNum = Math.floor(Math.random() * allMemeImgs.length);
         setRandomImg(allMemeImgs[randNum].url)
     }
+    const handleReset = (event) => {
+        event.preventDefault();
+        setTopText('');
+        setBottomText('');
+    }
     return(
-        <div>
-            <form className="meme-form" onSubmit={handleSubmit}>
-                <input 
-                    type="text" 
-                    name="topText" 
-                    placeholder="Top Text" 
-                    value={topText} 
-                    onChange={(e) => setTopText(e.target.value)}
-                />
-                <input 
-                    type="text" 
-                    name="bottomText" 
-                    placeholder="Bottom Text" 
-                    value={bottomText} 
-                    onChange={(e) => setBottomText(e.target.value)}
-                />
-                <button>Gen</button>
-            </form>
+        <div className="content-container">
+            <div>
+                <form className="meme-form" onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        className="text-input"
+                        name="topText" 
+                        placeholder="Top Text" 
+                        value={topText} 
+                        onChange={(e) => setTopText(e.target.value)}
+                    />
+                    <input 
+                        type="text" 
+                        className="text-input"
+                        name="bottomText" 
+                        placeholder="Bottom Text" 
+                        value={bottomText} 
+                        onChange={(e) => setBottomText(e.target.value)}
+                    />
+                    <button className="button">Change meme image</button>
+                    <button className="button" onClick={handleReset}>Reset</button>
+                </form>
+            </div>
             <div className='meme'>
                 <img  className="meme__img" src={randomImg} alt='' />
                 <h2 className='meme__top'>{topText}</h2>
